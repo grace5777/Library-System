2b0caca27c4d4c6ec5b127023ced502897a2cf22
grace5777 <161027943+grace5777@users.noreply.github.com>
2026-03-16 06:04:42 +0000

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+int main()
+{
+    Member m1(1, "Grace5777", "password", "grace@email.com",
+        "Grace", "High Wycombe", "07577006870", 20);
+
+    m1.login();
+
+    m1.menu(b1);
+
+    m1.viewProfile();
+
+    return 0;
+}
