e609a49d80a9dbd65848c2af9f2926f9f0c49cfd
grace5777 <161027943+grace5777@users.noreply.github.com>
2026-03-16 06:46:26 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,8 @@
 int main() {
     // declaring the variables for user to give details 
     int id;
-    int age;
     string username;
     string password;
-    string email;
-    string name;
-    string address;
-    string phone;
 
     // inputting the details 
     cout << "Please enter your ID: ";
@@ -19,23 +14,10 @@ int main() {
     cout << "Please enter your password: ";
     cin >> password;
 
-    cout << "Please enter your name: ";
-    cin >> name;
-
-    cout << "Please enter your email: ";
-    cin >> email;
-
-    cout << "Please enter your phone number: ";
-    cin >> phone;
-
-    cout << "Please enter your age: ";
-    cin >> age;
-
-
     Book b1("Book1", "Jane Doe", "Workbook");
 
     // making a new library member called m1 to be able to fill in the deatils
-    Member m1(id, username, password, email, name, address, phone, age);
+    Member m1(id, username, password);
 
     m1.login();
     m1.menu(b1);
